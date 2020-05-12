96f2bb31e94bb61cb9a898a835a9d33475c3f913
Arusli <andreworusli@gmail.com>
2020-05-12 18:21:32 -0400

    Add files via upload

---
+++ index.js
@@ -0,0 +1,29 @@
+// create hexcolor code generator
+// use dom to insert hexcolor to background color of body
+
+//creates an array of all hexcolor values
+const alphabet = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0, 'a', 'b', 'c', 'd', 'e', 'f'];
+
+// make random number between 1 and 16
+
+//generates full hexcolor code
+function generateHexcolor() {
+    let color = '#';
+    let randomInt = function() {
+        return Math.floor(Math.random() * 16)
+    }
+    for (i = 0; i<6; i++) {
+        color += alphabet[randomInt()];
+    }
+    document.getElementById('code').innerHTML = `${color}`;
+    return color;
+}
+
+
+
+let button = document.getElementById('button');
+let body = document.getElementById('body');
+button.addEventListener('click', function(){
+    body.style.backgroundColor = `${generateHexcolor()}`;
+});
+
